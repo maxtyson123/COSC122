5024105f09d6ce6d3d0aad5f116f491ad2efad87
Max Tyson <98maxt98@gmail.com>
2025-08-02 16:21:22 +1200

    Q1 - Nodes c)

--- COSC122/labs/lab3/working.py
+++ COSC122/labs/lab3/working.py
@@ -1,23 +1,35 @@
+from PythonTools.debug import capture_and_assert_file
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next_node = None
 
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
+def print_linked_list(start):
 
-# your answer should be two lines
-node1.next_node = node2
-node2.next_node = node3
+    node = start
+    while node:
+        print(node.data)
+        node = node.next_node
 
-# Check
-print('node1 ->', node1.data)
-print('node2 ->', node2.data)
-print('node3 ->', node3.data)
-print()
-print('Running through list:')
-print('node1 ->', node1.data, '->', end=' ')
-print(node1.next_node.data, '->', end=' ')
-print(node1.next_node.next_node.data, '->', end=' ')
-print(node1.next_node.next_node.next_node)
+
+# TESTING
+def t1():
+    head = Node(10)
+    head.next_node = Node(20)
+    head.next_node.next_node = Node(30)
+    head.next_node.next_node.next_node = Node(40)
+    print_linked_list(head)
+
+
+def t2():
+    start = Node('y')
+    start.next_node = Node('a')
+    start.next_node.next_node = Node('y')
+    start.next_node.next_node.next_node = Node('a')
+    start.next_node.next_node.next_node.next_node = Node('y')
+    print_linked_list(start)
+
+capture_and_assert_file(t1, "tests/t1.txt")
+capture_and_assert_file(t2, "tests/t2.txt")
