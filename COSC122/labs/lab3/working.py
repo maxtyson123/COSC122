032e63f3592028d7b1b21b876096dc97c0ea3a63
Max Tyson <98maxt98@gmail.com>
2025-08-02 15:26:38 +1200

    Q1 - Nodes b)

--- COSC122/labs/lab3/working.py
+++ COSC122/labs/lab3/working.py
@@ -5,9 +5,19 @@ class Node:
 
 node1 = Node(10)
 node2 = Node(20)
+node3 = Node(30)
 
-# your answer should be only one line
+# your answer should be two lines
 node1.next_node = node2
+node2.next_node = node3
 
-# Check that node2 is indeed now linked in after node1
-# check_answer()
+# Check
+print('node1 ->', node1.data)
+print('node2 ->', node2.data)
+print('node3 ->', node3.data)
+print()
+print('Running through list:')
+print('node1 ->', node1.data, '->', end=' ')
+print(node1.next_node.data, '->', end=' ')
+print(node1.next_node.next_node.data, '->', end=' ')
+print(node1.next_node.next_node.next_node)
